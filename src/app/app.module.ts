aef8b959d0ce40454a4e5a7660c8218e4d3f3a59
Thassanai2002 <thassanai.tangprom@gmail.com>
2024-09-12 17:25:57 +0700

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MenubarModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    HttpClientModule,
+    FormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
