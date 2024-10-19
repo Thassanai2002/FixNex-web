f8c2c1c972dd8eea7310c6d307dbb6eb1db93d56
Tamazon <taetaeamazon45@gmail.com>
2024-10-20 04:28:34 +0700

    Recommend Food Creation
    เริ่ม

---
+++ src/app/recommend-food/recommend-food.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommend-food',
+  templateUrl: './recommend-food.component.html',
+  styleUrl: './recommend-food.component.scss'
+})
+export class RecommendFoodComponent {
+
+}
