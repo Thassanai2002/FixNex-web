97d3b87647ace6febea295ff27a7e523691b77e9
Thassanai2002 <thassanai.tangprom@gmail.com>
2024-11-06 21:08:28 +0700

    asd

--- src/app/recommend-food/service/recfood.service.ts
+++ src/app/recommend-food/service/recfood.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Refood } from '../interface/refood';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecfoodService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getRecommendFood() {
+    return this.http.get<Refood>('http://localhost:3000/product');
+  }
 }
